f937dc8209626d1f691875f35fa56ce0f1db2c2c
Pet002 <pet.14336@gmail.com>
2023-11-13 23:45:12 +0700

    cd: fix cd build

--- main.go
+++ main.go
@@ -16,6 +16,29 @@ import (
 	"gorm.io/gorm"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
+
+		if c.Request.Method == "OPTIONS" {
+
+			c.AbortWithStatus(204)
+
+			return
+
+		}
+
+		c.Next()
+
+	}
+
+}
+
 var err error
 
 func main() {
@@ -48,7 +71,8 @@ func main() {
 		gin.SetMode(gin.ReleaseMode)
 	}
 	r := gin.Default()
-
+	// to set CORS
+	r.Use(CORSMiddleware())
 	// the clean arch
 	routes.CoinRoute(r, database.DB)
 	routes.PaymentSubscriptionRoute(r, database.DB)
